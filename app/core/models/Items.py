2484df1a286baeb8cb3add54cd41cc5a66fd9cd9
Danil <noskovde2015@gmail.com>
2025-12-04 15:34:52 +0300

    Create relationship Items with Users + add new migration

--- app/core/models/Items.py
+++ app/core/models/Items.py
@@ -1,11 +1,19 @@
-from sqlalchemy.orm import Mapped
-
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy.testing.schema import mapped_column
+from typing import TYPE_CHECKING
 from core.models.base import Base
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Item(Base):
     __tablename__ = "items"
-    name: Mapped[str]
-    description: Mapped[str]
-    price: Mapped[float]
-    category: Mapped[str]
+    name: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=True)
+    price: Mapped[float] = mapped_column(nullable=False)
+    category: Mapped[str] = mapped_column(nullable=False)
+
+    seller_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    seller: Mapped["User"] = relationship(back_populates="items")
