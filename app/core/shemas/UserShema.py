812802e713234e346a5f0b0ec911e5c0c0462f11
Danil <noskovde2015@gmail.com>
2025-12-05 21:59:20 +0300

    Create User Shema

---
+++ app/core/shemas/UserShema.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, Field, EmailStr, ConfigDict, field_validator
+
+
+class UserBase(BaseModel):
+    """
+    name: Mapped[str] = mapped_column(nullable=False)
+    surname: Mapped[str] = mapped_column(nullable=False)
+    nickname: Mapped[str] = mapped_column(unique=True, nullable=False)
+    age: Mapped[int] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    role: Mapped[UserRole] = mapped_column(default=UserRole.BUYER)
+
+    items: Mapped[list["Item"]] = relationship(back_populates="seller")
+    """
+
+    name: str = Field(max_length=25)
+    surname: str = Field(max_length=25)
+    nickname: str = Field(max_length=50)
+    age: int = Field(ge=0)
+    email: EmailStr
+
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserRead(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
