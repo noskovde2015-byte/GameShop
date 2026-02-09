052da75ffc35b63674edd074f588d5852db8f4b7
Danil <noskovde2015@gmail.com>
2026-02-09 12:06:04 +0300

    update items table: Add system_requirements and rating field

--- app/core/models/Items.py
+++ app/core/models/Items.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, Text, Float
 from sqlalchemy.orm import Mapped, relationship, mapped_column
 from typing import TYPE_CHECKING
 from core.models.base import Base
@@ -13,6 +13,8 @@ class Item(Base):
     description: Mapped[str] = mapped_column(nullable=True, default="")
     price: Mapped[float] = mapped_column(nullable=False)
     category: Mapped[str] = mapped_column(nullable=False)
+    system_requirements: Mapped[str] = mapped_column(Text, nullable=True, default="")
+    rating: Mapped[float] = mapped_column(Float, nullable=False, default=0.0)
 
     seller_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     seller: Mapped["User"] = relationship(back_populates="items")
