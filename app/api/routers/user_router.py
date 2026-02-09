12573d60d368e5e3ea7940f4badbf9157362773a
Danil <noskovde2015@gmail.com>
2026-02-09 12:05:15 +0300

    add become seller router

--- app/api/routers/user_router.py
+++ app/api/routers/user_router.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from core.models import db_helper
+from core.models import db_helper, User
 from core.config import settings
+from core.models.user import UserRole
 from core.shemas.UserShema import UserRead, UserCreate
 from api.crud.crud_func import get_users, create_user
+from api.dependencies import get_current_user
 
 
 router = APIRouter(prefix=settings.prefix.users, tags=["Users"])
 
 
+# Выдача всех пользователей
 @router.get("", response_model=list[UserRead])
 async def get_all_users(session: AsyncSession = Depends(db_helper.session_getter)):
     return await get_users(
@@ -16,6 +19,7 @@ async def get_all_users(session: AsyncSession = Depends(db_helper.session_getter
     )
 
 
+# Временный роутер для тестов
 @router.post("", response_model=UserCreate)
 async def create_new_user(
     user_data: UserCreate, session: AsyncSession = Depends(db_helper.session_getter)
@@ -24,3 +28,18 @@ async def create_new_user(
         session=session,
         user_data=user_data,
     )
+
+
+# Смена роли с покупателя на продавца
+@router.post("/become-seller")
+async def become_seller(
+    current_user: User = Depends(get_current_user),
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    if current_user.role == UserRole.SELLER:
+        return {"message": "Пользователь уже является продавцом"}
+
+    current_user.role = UserRole.SELLER
+    await session.commit()
+    await session.refresh(current_user)
+    return {"message": "Поздравляем! Вы успешно стали продавцом"}
