af52549938c1097baa7586c6b7cee7843c311183
Danil <noskovde2015@gmail.com>
2026-02-09 12:05:03 +0300

    Add dependencies for check seller

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -6,10 +6,12 @@ from starlette.status import (
     HTTP_409_CONFLICT,
     HTTP_401_UNAUTHORIZED,
     HTTP_404_NOT_FOUND,
+    HTTP_403_FORBIDDEN,
 )
 from jose import jwt, JWTError
 from core.models import db_helper, User
 from core.config import settings
+from core.models.user import UserRole
 
 
 async def get_current_user(
@@ -21,7 +23,7 @@ async def get_current_user(
     if not token:
         raise HTTPException(
             status_code=HTTP_401_UNAUTHORIZED,
-            detail="Token not found",
+            detail="Токен не найден",
         )
 
     try:
@@ -58,3 +60,11 @@ async def get_current_user(
             status_code=HTTP_401_UNAUTHORIZED,
             detail="Неверный токен",
         )
+
+
+async def require_seller(user: User = Depends(get_current_user)):
+    if user.role != UserRole.SELLER:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Пользователь не является продавцом"
+        )
+    return user
